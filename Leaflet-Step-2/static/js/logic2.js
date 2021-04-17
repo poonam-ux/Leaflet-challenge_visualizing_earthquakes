21ec62f96ec7e16f84ab30acfd23f1e033cfb5ec
poonam-ux <shaonam@gmail.com>
2021-04-17 07:51:49 -0700

    Add earthquakes markers

--- Leaflet-Step-2/static/js/logic2.js
+++ Leaflet-Step-2/static/js/logic2.js
@@ -50,3 +50,66 @@ var myMap = L.map("map", {
 
 // Create a Layer Control + Pass in baseMaps and overlayMaps + Add the Layer Control to the Map
 L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+// Retrieve earthquakesURL (USGS Earthquakes GeoJSON Data) with D3
+d3.json(earthquakesURL, function(earthquakeData) {
+    // Function to Determine Size of Marker Based on the Magnitude of the Earthquake
+    function markerSize(magnitude) {
+        if (magnitude === 0) {
+          return 1;
+        }
+        return magnitude * 3;
+    }
+
+    // Function to Determine Style of Marker Based on the Magnitude of the Earthquake
+    function styleInfo(feature) {
+        return {
+          opacity: 1,
+          fillOpacity: 1,
+          fillColor: chooseColor(feature.properties.mag),
+          color: "#000000",
+          radius: markerSize(feature.properties.mag),
+          stroke: true,
+          weight: 0.5
+        };
+    }
+
+    // Function to Determine Color of Marker Based on the Magnitude of the Earthquake
+    function chooseColor(magnitude) {
+		if (magnitude > 5) {
+			return "#ea2c2c";
+		}
+		if (magnitude > 4) {
+			return "#ea822c";
+		}
+		if (magnitude > 3) {
+			return "#ee9c00";
+		}
+		if (magnitude > 2) {
+			return "#eecc00";
+		}
+		if (magnitude > 1) {
+			return "#d4ee00";
+		}
+		return "#daf7a6";
+    }
+
+    // Create a GeoJSON Layer Containing the Features Array on the earthquakeData Object
+    L.geoJSON(earthquakeData, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        style: styleInfo,
+        // Function to Run Once For Each feature in the features Array
+        // Give Each feature a Popup Describing the Place & Time of the Earthquake
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h4>Location: " + feature.properties.place + 
+            "</h4><hr><p>Date & Time: " + new Date(feature.properties.time) + 
+            "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+        }
+
+    // Add earthquakeData to earthquakes LayerGroups 
+    }).addTo(earthquakes);
+    // Add earthquakes Layer to the Map
+    earthquakes.addTo(myMap);
+})
