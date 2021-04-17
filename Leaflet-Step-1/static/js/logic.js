efc8cec9f9db45cb030d0f7e3127e17fb1ccccd0
poonam-ux <shaonam@gmail.com>
2021-04-17 06:49:04 -0700

    Add legends

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -73,4 +73,30 @@ d3.json(geoData, function (data) {
             layer.bindPopup("Earthquake Magnitude: " + feature.properties.mag + "<br>Earthquake Location:<br>" + feature.properties.place);
         }
     }).addTo(myMap);
+
+    //add legend on Bottom Right Corner
+	var legend = L.control({ position: 'bottomright' });
+  
+	legend.onAdd = function (map) {
+        //Dom Utility that puts legend into DIV & Info Legend
+        var div = L.DomUtil.create('div', 'info legend'),
+        //Magnitude Grades, stops at 5 magnitude
+        grades = [0, 1, 2, 3, 4, 5];
+        
+        //Legend Label Earthquake <break> Magnitude
+        div.innerHTML = 'Eathquake<br>Magnitude<br><hr>'
+        
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+            //HTML code with nbs(non-breaking space) and ndash
+            '<i style="background:' + getColor(grades[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+
+    //Adds Legend to myMap
+    legend.addTo(myMap);
+    
 })
